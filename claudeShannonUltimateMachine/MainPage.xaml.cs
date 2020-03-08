2a137d9ce8c2d577fe4eb834b88224e60913ff2a
tanakakensaku <tannakaken@gmail.com>
2020-03-08 18:21:54 +0900

    first commit

---
+++ claudeShannonUltimateMachine/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace claudeShannonUltimateMachine
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnButtonClicked(object sender, EventArgs args)
+        {
+            Launcher.OpenAsync(new Uri("https://www.google.com"));
+        }
+    }
+}
